628dad20b9b2f5c554ff7efa08a1560162634fd2
Giordano Bruno <gio_braz@hotmail.com>
2018-08-21 15:21:10 -0300

    Add Projeto Marqueyde

---
+++ src/pages/list/list.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+
+import { NavController, NavParams } from 'ionic-angular';
+
+import { ItemDetailsPage } from '../item-details/item-details';
+
+@Component({
+  selector: 'page-list',
+  templateUrl: 'list.html'
+})
+export class ListPage {
+  icons: string[];
+  items: Array<{title: string, note: string, icon: string}>;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.icons = ['contact', 'contact', 'contact', 'contact', 'contact'];
+
+    this.items = [];
+    for(let i = 1; i < 6; i++) {
+      this.items.push({
+        title: 'Paciente ' + i,
+        note: 'Prontuário: ' + i,
+        icon: this.icons[Math.floor(Math.random() * this.icons.length)]
+      });
+    }
+  }
+
+  itemTapped(event, item) {
+    this.navCtrl.push(ItemDetailsPage, {
+      item: item
+    });
+  }
+}
